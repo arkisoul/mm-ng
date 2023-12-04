ed1d4c16df8340617295588c310b13e67c65ab1a
arkisoul <arpit_jain@live.com>
2023-12-04 19:22:53 +0530

    (feat) init base components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HelloWorldComponent],
+  // template: `<h1>Hello, world!</h1>`,
+  // styles: ['h1{color: tomato;}', '', ''],
+  // styleUrls: ['./app.component.css', './app.component.css']
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'todoapp';
---
+++ src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,10 @@
+import { Component, NgModule } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  standalone: true,
+  imports: [],
+  templateUrl: './hello-world.component.html',
+  styleUrl: './hello-world.component.css',
+})
+export class HelloWorldComponent {}
